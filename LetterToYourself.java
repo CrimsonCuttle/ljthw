cdb86308c0989a0393bd5ca401520642544edf65
MrCodingFan <oosmimoo@gmail.com>
2015-07-05 19:19:42 -0500

    Added a smiley face to greet you!

---
+++ LetterToYourself.java
@@ -0,0 +1,15 @@
+public class LetterToYourself
+{
+	public static void main( String[] args )
+	{
+		System.out.println( "( () )  ( () )" );
+		System.out.println( "      /_      " );
+		System.out.println( "\\_          _/" );
+		System.out.println( "  \\________/  " );
+		System.out.println( "              " );
+		System.out.println( "+-/\\---------+" );
+		System.out.println( "| Hello! How |" );
+		System.out.println( "| are you?   |" );
+		System.out.println( "+------------+" );
+	}
+}
