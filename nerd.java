e27b385044b2ff03698e9dd8ece92bdc5f757b89
MrCodingFan <oosmimoo@gmail.com>
2015-07-11 16:35:04 -0500

    Who's a nerd? (hint: b_r_)

--- nerd.java
+++ nerd.java
@@ -2,15 +2,34 @@ import java.util.Scanner;
 
 public class nerd
 {
-	public static void main( String[] args )
+	public static void main( String[] args ) throws InterruptedException
 	{
 		String nerd;
+		int bird;
 
 		Scanner keyboard = new Scanner(System.in);
 
 		System.out.println( "Who's a nerd?" );
 		nerd = keyboard.next();
 
-		System.out.println( nerd + " is a nerd." );
+		if ( nerd.equals("bird") )
+		{
+			bird = 1;
+			System.out.println( "That is the WORD." );
+			Thread.sleep(1000);
+			System.out.println( "BIRD BIRD BIRD" );
+			Thread.sleep(1000);
+			System.out.println( "BIRD IS THE WORD" );
+			Thread.sleep(1000);
+			while ( bird == 1 )
+			{
+				System.out.println( "BIRD" );
+				Thread.sleep(500);
+			}
+		}
+		else
+		{
+			System.out.println( nerd + " is a nerd. " );
+		}
 	}
 }
