874ecd4340d96db5234dd1a81fc116783f8bf61d
MrCodingFan <oosmimoo@gmail.com>
2015-07-19 15:22:24 -0500

    More stuff, like rolling dice and whatnot.

---
+++ PigDicePlayable.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class PigDicePlayable
+{
+	public static void main(String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+
+		int roll, total1, total2, turnTotal;
+		String choice = "";
+
+		total1 = 0;
+		total2 = 0;
+
+		do
+		{
+			turnTotal = 0;
+			System.out.println("You have " + total1 + " points.");
+
+			do
+			{
+				roll = 1 + (int)(Math.random()*6);
+				System.out.println("\tYou rolled a " + roll + ".");
+				if (roll == 1)
+				{
+					System.out.println("\tThat ends your turn.");
+					turnTotal = 0;
+				}
+				else
+				{
+					turnTotal += roll;
+					System.out.println("\tYou have " + turnTotal + " points this round.");
+					System.out.print("\tWould you like to 'roll' again or 'hold?'");
+					choice = keyboard.next();
+				}
+			} while (roll != 1 && choice.equals("roll"));
+
+			total1 += turnTotal;
+			System.out.println( "\tYou end the round with " + total1 + " points.");
+			if (total1 < 100 )
+			{
+				turnTotal = 0;
+				System.out.println("Computer has " + total2 + " points.");
+				do
+				{
+					roll = 1 + (int)(Math.random()*6);
+					System.out.println("\tComputer rolled a " + roll + ".");
+					if (roll == 1)
+					{
+						System.out.println("\tThat ends its turn.");
+						turnTotal = 0;
+					}
+					else
+					{
+						turnTotal += roll;
+						System.out.println("\tComputer has " + turnTotal + " points this round.");
+						if (turnTotal <20)
+						{
+							System.out.println("Computer chooses to roll again.");
+						}
+					}
+				} while ( roll != 1 && turnTotal < 20);
+
+				total2 += turnTotal;
+				System.out.println("\tComputer ends the round with " + total2 + " points.");
+			}
+		} while (total1 < 100 && total2 < 100 );
+
+		if (total1 > total2)
+		{
+			System.out.println("You win!");
+		}
+		else
+		{
+			System.out.println("Computer wins!");
+		}
+	}
+}
