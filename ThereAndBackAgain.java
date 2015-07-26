5bb36b30c7309e5db14160fc94f7ee832da5b035
Chander Hansen <chander@twerp>
2015-07-25 21:02:31 -0500

    more things

---
+++ ThereAndBackAgain.java
@@ -0,0 +1,16 @@
+public class ThereAndBackAgain
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Here.");
+		poopy();
+		System.out.println("Back first time.");
+		poopy();
+		System.out.println("Back second language.");
+	}
+	
+	public static void poopy()
+	{
+		System.out.println("There.");
+	}
+}
